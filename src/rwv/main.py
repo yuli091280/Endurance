a4b9dc8aca08314ed529c5c60ae4eba80af3d3fd
Yuchao Li <94849935+yuli091280@users.noreply.github.com>
2023-11-01 21:04:50 -0400

    RWV-2 add db file commandline args

--- src/rwv/main.py
+++ src/rwv/main.py
@@ -6,10 +6,16 @@ import sqlite3
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from rwv.main_window import MainWindow
-
+from rwv.judge_data_base import JudgeDatabase
 
 def main():
     app = QApplication(sys.argv)
+
+    if len(sys.argv) != 2:
+        print("no db file provided")
+        return 1
+
+    JudgeDatabase(sys.argv[1])
 
     window = MainWindow()
     window.show()
