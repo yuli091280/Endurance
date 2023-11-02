fc868597663e68bc5bdb28ce2aceda25479b65c0
Yuchao Li <94849935+yuli091280@users.noreply.github.com>
2023-11-01 21:56:54 -0400

    RWV-20 add sanity check before using file

--- src/rwv/main.py
+++ src/rwv/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import sqlite3
 
 from PyQt6.QtWidgets import QApplication, QMainWindow
@@ -9,12 +10,24 @@ from rwv.main_window import MainWindow
 from rwv.judge_data_base import JudgeDatabase
 
 
-def main():
+def start_db():
     if len(sys.argv) != 2:
         print("no db file provided")
+        return None
+
+    db_path = sys.argv[1]
+    if not os.path.isfile(db_path):
+        print("bad file path")
+        return None
+
+    return JudgeDatabase(sys.argv[1])
+
+
+def main():
+    db = start_db()
+    if db is None:
         return 1
 
-    db = JudgeDatabase(sys.argv[1])
     print(db.judgeById(2))
     print(db.athleteByBib(111))
     print(db.raceById(1))
