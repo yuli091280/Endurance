23d1de125d4d829dd2dc2b964b22b3f5111f8b3a
Andrew Rodebaugh <ajr439@drexel.edu>
2024-02-26 14:29:41 -0500

    fix comments

--- test_plot_widget.py
+++ test_plot_widget.py
@@ -1,20 +1,12 @@
-import pytest
-from unittest.mock import Mock
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import QApplication
 from rwv.ui.plot_widget import PlotWidget
 from rwv.db import DB
-from PyQt6 import QtWidgets, QtCore
-
-
-import pytest
-from PyQt6.QtWidgets import QApplication
-from rwv.ui.plot_widget import PlotWidget
-from rwv.db import DB
-import sys
 from rwv.ui.main_window import MainWindow
 
+
 def test_plot_widget_user_interaction(qtbot):
-    app = QApplication(sys.argv)
+    app = QApplication([])
 
     window = MainWindow(app.primaryScreen())
 
@@ -30,5 +22,9 @@ def test_plot_widget_user_interaction(qtbot):
     # Simulate clicking 60 ms
     widget.max_loc_combo_box.setCurrentIndex(1)
 
-    # Simulate double clicking on the first item of the left list of the DoubleListWidget
+    # Simulate double clicking on the first item of the left list of the runner list
     qtbot.mouseDClick(widget.runner_list._left_list.viewport(), QtCore.Qt.MouseButton.LeftButton)
+
+    # Simulate double clicking on the first item of the left list of the judge list
+    qtbot.mouseDClick(widget.judge_list._left_list.viewport(), QtCore.Qt.MouseButton.LeftButton)
+
