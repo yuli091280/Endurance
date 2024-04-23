558d0d22bdbda0b5310c75575c6ea47843fe9167
Manish Patel <mtp75@drexel.edu>
2024-04-23 19:05:01 -0400

    Save Menu and functionality in graph window.

--- src/rwv/ui/graph_window.py
+++ src/rwv/ui/graph_window.py
@@ -1,5 +1,5 @@
 from PyQt6 import QtWidgets
-
+from PyQt6.QtWidgets import QFileDialog
 
 class GraphWindow(QtWidgets.QWidget):
     """A window that displays a generated chart.
@@ -12,32 +12,82 @@ class GraphWindow(QtWidgets.QWidget):
 
     def __init__(self, toolbar, canvas, show_graph_button):
         super().__init__()
-
         self.setWindowTitle("Endurance")
         self.show_graph_button = show_graph_button
+        self.toolbar = toolbar
+        self.canvas = canvas
 
         layout = QtWidgets.QVBoxLayout(self)
+
+        # Setup the menu bar
+        menu_bar = QtWidgets.QMenuBar()
+        file_menu = QtWidgets.QMenu("&File", self)
+        menu_bar.addMenu(file_menu)
+
+        # Add actions to the file menu
+        save_graph = file_menu.addAction("Save Graph")
+        save_graph.triggered.connect(self.save_current_graph)
+        save_graph.setShortcut('Ctrl+S')
+
+
+        exit_action = file_menu.addAction("Close Graph")
+        exit_action.triggered.connect(self.close_application)
+        exit_action.setShortcut('Ctrl+Q')
+
+        layout.setMenuBar(menu_bar)
         layout.addWidget(toolbar)
         layout.addWidget(canvas)
-
         self.setLayout(layout)
 
-    def closeEvent(self, event):
+    def save_current_graph(self, checked=False):
         """
-        Overrides the closeEvent function to close the window and show the show graph button.
+        Opens window for the user to save the current graph as PDF or JPEG.
         """
-        self.show_graph_button.show()
-        self.close()
-        event.accept()
+        file_path, save_choice = QFileDialog.getSaveFileName(
+            self, "Save Graph", "", "PDF Files (*.pdf);;JPEG Files (*.jpeg;*.jpg)"
+        )
+        if file_path:
+            if "PDF" in save_choice and not file_path.endswith(".pdf"):
+                file_path += ".pdf"
+            elif ("JPEG" in save_choice or "jpg" in save_choice) and not file_path.endswith((".jpeg", ".jpg")):
+                file_path += ".jpeg"
 
-    def show_window(self):
+            if "PDF" in save_choice:
+                self.save_figure_as_pdf(file_path)
+            elif "JPEG" in save_choice:
+                self.save_figure_as_jpeg(file_path)
+
+
+    def save_figure_as_pdf(self, file_path):
         """
-        Shows the window.
+        Saves the graph as a pdf at a file path.
+
+        :param file_path: The file path to save the pdf to
+        :type file_path: str
         """
-        self.show()
+        self.canvas.figure.savefig(file_path)
+
+    def save_figure_as_jpeg(self, file_path):
+        """
+        Saves the graph as a jpeg at a file path.
+
+        :param file_path: The file path to save the jpeg to
+        :type file_path: str
+        """
+        self.canvas.figure.savefig(file_path, format='jpeg')
+
+    def close_application(self, checked=False):
+        """
+        Closes the entire graph window.
+        """
+        self.close()
+
 
     def close_window(self):
-        """
-        Hides the window.
-        """
+        ""
+        self.show_graph_button.show()
         self.close()
+
+    def show_window(self):
+        ""
+        self.show()
